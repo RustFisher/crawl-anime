2e520548fb7b54b4b084f45a92915a92ea60d7d4
Rustfisher <rustfisher@foxmail.com>
2017-06-01 21:37:53 +0800

    get info persons

--- bang/bang/spiders/anime_spider.py
+++ bang/bang/spiders/anime_spider.py
@@ -1,11 +1,16 @@
 import scrapy
 
+''' 获取动画的信息 '''
+
 
 class AnimeSpider(scrapy.Spider):
     name = "bang"
 
     start_urls = [
-        'file://H/fisher_p/crawl-anime/web_source/a118335.html',
+        'file:///home/rust/ws/crawl-anime/web_source/a118335.html',
+        'file:///home/rust/ws/crawl-anime/web_source/a210811.html',
+        'file:///home/rust/ws/crawl-anime/web_source/a146732.html',
+        # 'file://H/fisher_p/crawl-anime/web_source/a118335.html',
     ]
 
     def parse(self, response):
@@ -15,25 +20,52 @@ class AnimeSpider(scrapy.Spider):
         title = res.css('h1.nameSingle')
         name_jp = title.css('a::text').extract_first()
         a_link = title.css('a::attr(href)').extract_first()
-        bang_id = str(a_link).split('/')[-1]
+        anime_bang_id = str(a_link).split('/')[-1]
         name_zh = title.css('a::attr(title)').extract_first()
         sub_title = title.css('small::text').extract_first()
-        print(name_jp + " , " + name_zh + " , " + bang_id + " , " + sub_title)
+        print(name_jp + " , " + name_zh + " , " + anime_bang_id + " , " + sub_title)
 
-        # 获取信息表
-        info = res.css('div.infobox')
+        summary = ""  # 获取简介
+        for s in res.css('div.subject_summary::text').extract():
+            summary += s
+        print(summary)
+
+        info = res.css('div.infobox')  # 获取左侧信息表
 
         pic_link = info.css('a::attr(href)').extract_first()  # 获取封面图片地址
         print("pic link: " + pic_link)
+
+        info_list = []  # 存储左侧信息
         for li in info.css('li'):
             info_title = li.css('span.tip::text').extract_first()
             info_content_href = li.css('a').extract_first()
+            list_entity = []
+            staff_list = []
             if info_content_href is None:
-                info_contents = li.css('li::text').extract_first()
+                staff_list.append(li.css('li::text').extract_first())
             else:
-                # 若是链接  将其中的信息提取拼接成一个字符串
-                people_bang_id = str(li.css('a::attr(href)').extract_first()).split("/")[-1]
-                info_contents = li.css('a::text').extract_first() + "|" + people_bang_id
-            print(info_title + " " + str(info_contents))
+                # 若是链接而且可能有多个链接  将其中的信息提取拼接
+                for p in li.css('a'):
+                    p_id = p.xpath('@href').extract_first().split("/")[-1]
+                    p_name = p.css('a::text').extract_first()
+                    staff_list.append(p_name + "%%" + p_id)
 
-        print("Finish")
+            list_entity.append(info_title)
+            list_entity.append(staff_list)
+            info_list.append(list_entity)
+
+        for le in info_list:
+            print(le)
+
+        chars = res.css('div.userContainer')  # get characters and CV
+        char_list = []
+        for char in chars:
+            character_bang_id = char.css('a::attr(href)').extract_first().split("/")[-1]
+            character_name = char.css('a::attr(title)').extract_first()
+            cv_bang_id = char.css('a::attr(href)')[-1].extract().split("/")[-1]
+            cv_name = char.css('a::text')[-1].extract()
+            char_list.append(character_name + "%%" + character_bang_id + "%&" + cv_name + "%%" + cv_bang_id)
+
+        print(char_list)
+
+        print("Finish\n")
