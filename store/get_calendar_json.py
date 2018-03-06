ec90c25b4a1b04cb953a8ac8a65c7d46aa87b7f4
rustfisher <rustfisher@foxmail.com>
2018-03-06 07:41:18 -0800

    fix bug get_calendar_json

--- store/get_calendar_json.py
+++ store/get_calendar_json.py
@@ -17,14 +17,21 @@ def get_calendar_json(season_id):
         "where update_in_week_str is not null and season_id = '" + season_id + "'")
     res_all = cursor.fetchall()
     print("Find data count is %s" % len(res_all))
-    res_dict = []
+    res_dict_list = []
     for i in range(0, len(res_all)):
-        res_dict.append(res_all[i])
-    print(res_dict)
+        item = res_all[i]
+        json_item = dict()
+        json_item["anime_bang_id"] = item[0]
+        json_item["name_zh"] = item[1]
+        json_item["name_jp"] = item[2]
+        json_item["update_in_week"] = item[3]
+        json_item["update_in_week_str"] = item[4]
+        res_dict_list.append(json_item)
+    print(res_dict_list)
     cursor.close()
     conn.close()
     json_path = os.path.join(calendar_dir, "calendar_" + season_id + ".json")
-    codecs.open(json_path, "w", "utf-8").write(json.dumps(res_dict, ensure_ascii=False))
+    codecs.open(json_path, "w", "utf-8").write(json.dumps(res_dict_list, ensure_ascii=False))
 
 
 if __name__ == "__main__":
